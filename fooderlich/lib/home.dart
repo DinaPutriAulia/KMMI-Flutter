12be72913f09b314e0b621be4d566ee96291e250
DinaPutriAulia <nazaria20162016@gmail.com>
2021-09-14 19:47:37 +0700

    commit 1

---
+++ fooderlich/lib/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/explore_screen.dart';
+import 'screens/recipes_screen.dart';
+import 'screens/grocery_screen.dart';
+import 'screens/search_screen.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    RecipesScreen(),
+    const GroceryScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title:
+              Text('Fooderlich', style: Theme.of(context).textTheme.headline6),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showSearch(context: context, delegate: Search());
+                },
+                icon: const Icon(Icons.search))
+          ],
+        ),
+        body: pages[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor:
+                Theme.of(context).textSelectionTheme.selectionColor,
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                  icon: Icon(Icons.explore), label: 'Explore'),
+              const BottomNavigationBarItem(
+                  icon: Icon(Icons.book), label: 'Recipes'),
+              const BottomNavigationBarItem(
+                  icon: Icon(Icons.list), label: 'To Buy'),
+            ]));
+  }
+}
